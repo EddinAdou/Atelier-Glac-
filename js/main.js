da85d89a5af00a3cf268a522d42a3e10f5bcd1d2
EddinAdou <k.adou@it-students.fr>
2025-01-11 23:47:07 +0100

    maj title

--- js/main.js
+++ js/main.js
@@ -360,4 +360,20 @@ document.addEventListener('DOMContentLoaded', function () {
 	window.addEventListener('scroll', checkScroll);
 	checkScroll(); // Vérifiez l'état initial
   });
+
+
+  document.querySelectorAll('.mouse-icon').forEach((icon) => {
+	icon.addEventListener('click', function (e) {
+	  e.preventDefault();
+	  const targetId = this.getAttribute('href').slice(1);
+	  const targetElement = document.getElementById(targetId);
+	  if (targetElement) {
+		window.scrollTo({
+		  top: targetElement.offsetTop,
+		  behavior: 'smooth',
+		});
+	  }
+	});
+  });
+  
   
