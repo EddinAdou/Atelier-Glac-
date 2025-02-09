c4ff1cab77544f433f13e4e9e4bad124c56bc903
EddinAdou <k.adou@it-students.fr>
2025-02-09 23:19:01 +0100

    maj

--- js/main.js
+++ js/main.js
@@ -375,5 +375,18 @@ document.addEventListener('DOMContentLoaded', function () {
 	  }
 	});
   });
-  
-  
+ const slider = document.querySelector('.slider');
+ const nextButton = document.getElementById('next');
+ const prevButton = document.getElementById('prev');
+
+ let index = 0;
+
+ nextButton.addEventListener('click', () => {
+	 index = (index + 1) % slider.children.length; // Boucle infinie
+	 slider.style.transform = `translateX(-${index * 100}%)`;
+ });
+
+ prevButton.addEventListener('click', () => {
+	 index = (index - 1 + slider.children.length) % slider.children.length;
+	 slider.style.transform = `translateX(-${index * 100}%)`;
+ });
